00ca9c16c152b0e9db6ecb14eb8a60ac5ff66973
Alyshay98 <97034708+Alyshay98@users.noreply.github.com>
2024-01-16 11:21:23 -0700

    Working!
    Everything is working!!!!

--- MyPortfolio/PortfolioDatabase/urls.py
+++ MyPortfolio/PortfolioDatabase/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import home, hobbies, portfolio, contact, hobby_detail, portfolio_detail, about
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', home, name='home'),
@@ -11,3 +13,6 @@ urlpatterns = [
     path('portfolio/<int:portfolio_id>/', portfolio_detail, name='portfolio_detail'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
