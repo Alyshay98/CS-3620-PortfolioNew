dcd7fd06d8ae57793b62a9b936afae448e7fea44
Alyshay98 <97034708+Alyshay98@users.noreply.github.com>
2024-01-17 13:24:54 -0700

    Users and register
    Added the users app and register.html

--- MyPortfolio/PortfolioDatabase/admin.py
+++ MyPortfolio/PortfolioDatabase/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Portfolio, Hobby
+from .models import Portfolio, Hobby, Contact
 
 # Register your models here.
 
 admin.site.register(Portfolio)
 admin.site.register(Hobby)
+admin.site.register(Contact)
--- MyPortfolio/PortfolioDatabase/urls.py
+++ MyPortfolio/PortfolioDatabase/urls.py
@@ -2,7 +2,8 @@ from django.urls import path, include
 from .views import home, hobbies, portfolio, contact, hobby_detail, portfolio_detail, about
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+from users import views as user_views
+from django.contrib.auth import views as authentication_views
 
 urlpatterns = [
     path('', home, name='home'),
@@ -12,8 +13,9 @@ urlpatterns = [
     path('contact/', contact, name='contact'),
     path('hobbies/<int:hobby_id>/', hobby_detail, name='hobby_detail'),
     path('portfolio/<int:portfolio_id>/', portfolio_detail, name='portfolio_detail'),
-    path('login/', auth_views.LoginView.as_view(template_name='PortfolioDatabase/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', user_views.register, name='register'),
+
+
 
 ]
 
